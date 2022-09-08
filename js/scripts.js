4b2f2685fa41e01780b03286c1d227ae9192db8e
sk <sk@typedivision.d>
2022-09-08 21:00:21 +0200

    cuw

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*!
+* Start Bootstrap - Bare v5.0.8 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+function rsvp() {
+  $('#rsvp').show();
+}
+
+function rsvpok() {
+  $('#rsvp').hide();
+}
